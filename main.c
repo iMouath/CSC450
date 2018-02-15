e1c34ea1ef299e2aed36c7d539a09b06e33adece
Mouath Ibrahim <Mouath.Ibrahim@gmail.com>
2018-02-15 10:16:30 -0600

    HW3

--- main.c
+++ main.c
@@ -1,49 +1,38 @@
-#include <stdio.h>
+//
+// Created by mouath on 2/14/18.
+//
+
 #include <stdlib.h>
-
-int *insertionSort(int array[]) {
-    int i, temp, j;
-    for (i = 1; i < 1000; i++) {
-        temp = *(array + i);
-        j = i - 1;
-
-        while (j >= 0 && *(array + j) > temp) {
-            *(array + j + 1) = *(array + j);
-            j = j - 1;
-        }
-        *(array + j + 1) = temp;
-    }
-    return array;
-}
-
-void printArray(int array[]) {
-    int i;
-    for (i = 0; i < 1000; i++) {
-        printf("%d\n", *(array + i));
-    }
-}
+#include <stdio.h>
 
 int main(int argc, char **argv) {
-    /* Create an array of 1000 integers using malloc
-     * fill the array with random numbers using the rand() function from stdlib.h
-     * write an Insertion sort (https://en.wikipedia.org/wiki/Insertion_sort).
-     * Output the unsorted and sorted array.
-     * */
 
-    int *intArray = (int *) malloc(1000 * sizeof(int));
-    int i;
-    for (i = 0; i < 1000; i++) {
-        *(intArray + i) = rand() % 1001;
-    }
-    printf("Unsorted Array\n");
-
-    printArray(intArray);
-
-    int *sortedIntArray = insertionSort(intArray);
-
-    printf("Sorted Array\n");
-    printArray(sortedIntArray);
+    char *s = (char *) malloc(50 * sizeof(char));
+//    s = "Hello";
+    printf("Please Enter something: ");
+    scanf("%s", s);
+    printf("%s\n", s);
 
     return 0;
+}
+
+//returns the pos of the 1st occurrence of c in s or -1 if not found
+int indexOf(char *s, int length, char c) {
+
+}
+
+int charAt(char *s, int length, int index) {
+
+}
+
+char *reverseString(char *s, int length) {
+
+}
+
+void reverseStringInPlace(char *s, int length) {
+
+}
+
+char *removeVowels(char *s, int length) {
 
 }
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by mouath on 2/15/18.
+//
+
+#ifndef UNTITLED_MAIN_H
+#define UNTITLED_MAIN_H
+
+#endif //UNTITLED_MAIN_H
